21f18cd6df7f48fca1e4440d9f9f0dbc31fb64c7
unknown <m7mdalhaj22@gmail.com>
2024-08-18 02:24:26 -0700

    almasafertest

---
+++ src/homePage/MyTestCases.java
@@ -0,0 +1,77 @@
+package homePage;
+
+import java.time.Duration;
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MyTestCases {
+	
+	
+	WebDriver driver = new ChromeDriver();
+	String AlmosaferURL = "https://global.almosafer.com/en";
+	Random rand = new Random();
+    String ExpectedDefaultLanage = "en";
+    
+    
+    
+    @BeforeTest
+    public void mySetup() {
+    	driver.manage().window().maximize();
+    	
+    	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    	
+    	driver.get(AlmosaferURL);
+    	
+    	driver.findElement(By.cssSelector(".sc-jTzLTM.hQpNle.cta__button.cta__saudi.btn.btn-primary")).click();
+    		
+    	
+    	
+    }
+    
+    @Test(priority = 1)
+    public void CheckTheDefaultLangugeIsEnglish() {
+    	
+    	String ActualLanguage = driver.findElement(By.tagName("html")).getAttribute("lang");
+    	Assert.assertEquals(ActualLanguage, ExpectedDefaultLanage);
+    	
+    }
+    
+	@Test(priority = 2)
+	public void CheckdefaultCurrency() {
+		
+		String ExpectedCurrency = "SAR";
+		
+		WebElement Currency = driver.findElement(By.xpath("//button[@data-testid='Header__CurrencySelector']"));
+		String ActualCurrency = Currency.getText();
+		Assert.assertEquals(ActualCurrency, ExpectedCurrency);
+	}
+	
+	@Test(priority = 3)
+	public void CheckContactNumber() {
+		
+		String ExpectedContactNumber = "+966554400000";
+		
+		String ActualContactNumber = driver.findElement(By.tagName("strong")).getText();
+		Assert.assertEquals(ActualContactNumber, ExpectedContactNumber);
+		
+	}
+	@Test(priority = 4)
+	public void CheckQitagLogo() {
+		
+		boolean ExpectedResultsForTheLogo = true;
+		WebElement theFooter = driver.findElement(By.tagName("footer"));
+		WebElement logo = theFooter.findElement(By.cssSelector(".sc-fihHvN.eYrDjb"))
+				.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ekulBa.eYboXF"));
+		
+		boolean ActualResultForThelogo = logo.isDisplayed();
+		Assert.assertEquals(ActualResultForThelogo, ExpectedResultsForTheLogo);
+	}
+
+}
